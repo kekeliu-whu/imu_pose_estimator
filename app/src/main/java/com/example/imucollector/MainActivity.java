0520e9ba2adfd37ba4450cdaebb694495ea25c1d
kekliu <kekliu@outlook.com>
2021-08-14 08:18:52 +0800

    add sensor listener

--- app/src/main/java/com/example/imucollector/MainActivity.java
+++ app/src/main/java/com/example/imucollector/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.imucollector;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    double ax, ay, az;   // these are the acceleration in x,y and z axis
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER_UNCALIBRATED), SensorManager.SENSOR_DELAY_FASTEST);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE_UNCALIBRATED), SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER_UNCALIBRATED) {
+            ax = event.values[0];
+            ay = event.values[1];
+            az = event.values[2];
+            Log.w("TAG", "acc: " + event.timestamp + " " + event.values.length);
+        }
+        if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE_UNCALIBRATED) {
+            ax = event.values[0];
+            ay = event.values[1];
+            az = event.values[2];
+            Log.w("TAG", "gyro: " + event.timestamp + ", " + event.values.length);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
