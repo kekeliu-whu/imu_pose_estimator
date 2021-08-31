3de5c9d9072cbaad57603d74034f5937445694d2
kekeliu <rickyrickc132@gmail.com>
2021-09-01 00:02:02 +0800

    extract estimator interface

---
+++ 3-ImuESKF/common.h
@@ -0,0 +1,9 @@
+#ifndef IMU_ESKF_COMMON_H
+#define IMU_ESKF_COMMON_H
+
+#include <Eigen/Eigen>
+
+using Quaterniond = Eigen::Quaterniond;
+using Vec3d = Eigen::Vector3d;
+
+#endif //IMU_ESKF_COMMON_H
---
+++ 3-ImuESKF/estimator.h
@@ -0,0 +1,31 @@
+#ifndef IMU_ESKF_ESTIMATOR_H
+#define IMU_ESKF_ESTIMATOR_H
+
+#include "common.h"
+
+class Estimator {
+public:
+    Estimator() = default;
+
+    virtual ~Estimator() = default;
+
+    virtual Quaterniond EstimatePose(double timestamp, const Vec3d &ang, const Vec3d &acc) = 0;
+
+
+public:
+    double get_last_timestamp() const {
+        return last_timestamp;
+    }
+
+    bool get_init_ok() const {
+        return init_ok;
+    }
+
+
+protected:
+    bool init_ok;
+    Quaterniond pose;
+    double last_timestamp;
+};
+
+#endif //IMU_ESKF_ESTIMATOR_H
