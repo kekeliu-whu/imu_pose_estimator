5dd3a2b142eaee1c56a654001002bdfafc592e22
kekliu <kekliu@outlook.com>
2021-09-04 23:59:08 +0800

    support google test

--- 3-ImuESKF/ekf_estimator.cc
+++ 3-ImuESKF/ekf_estimator.cc
@@ -1,1 +1,11 @@
 #include "ekf_estimator.h"
+
+Quaterniond EkfEstimator::EstimatePose(double timestamp, const Vec3d &ang, const Vec3d &acc) {
+    if (!this->init_ok) {
+        this->pose = Quaterniond::Identity();
+        this->init_ok = true;
+        this->last_timestamp = timestamp;
+        return this->pose;
+    }
+    return this->pose;
+}
---
+++ 3-ImuESKF/utils_test.cc
@@ -0,0 +1,27 @@
+#include "utils.h"
+#include <gtest/gtest.h>
+
+const double kEpsilon = 1e-8;
+
+TEST(UtilsTest, Ql) {
+    auto q1 = Quaterniond::UnitRandom();
+    auto q2 = Quaterniond::UnitRandom();
+
+    Eigen::Vector4d ans1 = Ql<double>(q1) * q2.coeffs();
+    Eigen::Vector4d ans2 = (q1 * q2).coeffs();
+    EXPECT_LT((ans1 - ans2).norm(), kEpsilon);
+}
+
+TEST(UtilsTest, Qr) {
+    auto q1 = Quaterniond::UnitRandom();
+    auto q2 = Quaterniond::UnitRandom();
+
+    Eigen::Vector4d ans1 = Qr<double>(q2) * q1.coeffs();
+    Eigen::Vector4d ans2 = (q1 * q2).coeffs();
+    EXPECT_LT((ans1 - ans2).norm(), kEpsilon);
+}
+
+//int main(int argc, char **argv) {
+//    ::testing::InitGoogleTest(&argc, argv);
+//    return RUN_ALL_TESTS();
+//}
