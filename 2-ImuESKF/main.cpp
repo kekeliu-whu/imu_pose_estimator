1ead9df23a392451974afc2cdaf3ade8926824de
kekeliu <rickyrickc132@gmail.com>
2022-08-09 15:19:53 +0800

    add flag 'filter_type' to ros executable

--- 2-ImuESKF/main.cpp
+++ 2-ImuESKF/main.cpp
@@ -4,6 +4,7 @@
 #include "estimator/simple_estimator.h"
 
 #include <geometry_msgs/PoseStamped.h>
+#include <gflags/gflags.h>
 #include <glog/logging.h>
 #include <iostream>
 #include <ros/ros.h>
@@ -14,8 +15,25 @@
 #include <tf2_ros/static_transform_broadcaster.h>
 #include <thread>
 
+DEFINE_string(filter_type, "", "Type of the filter, value must be one of sf/ekf/eskf/cf.");
+
 std::unique_ptr<tf2_ros::StaticTransformBroadcaster> static_broadcaster;
-std::unique_ptr<Estimator> estimator = std::make_unique<EskfEstimator>();
+std::unique_ptr<Estimator> estimator;
+
+void InitEstimatorOrExit() {
+  if (FLAGS_filter_type == "sf") {
+    estimator = std::make_unique<SimpleEstimator>();
+  } else if (FLAGS_filter_type == "ekf") {
+    estimator = std::make_unique<EkfEstimator>();
+  } else if (FLAGS_filter_type == "eskf") {
+    estimator = std::make_unique<EskfEstimator>();
+  } else if (FLAGS_filter_type == "cf") {
+    estimator = std::make_unique<ComplementaryFilterEstimator>();
+  } else {
+    std::cout << "Filter type '" << FLAGS_filter_type << "' is not one of sf/ekf/eskf/cf." << std::endl;
+    exit(1);
+  }
+}
 
 void chatterCallback(const sensor_msgs::Imu::ConstPtr &imu_ptr) {
   auto cur_timestamp = imu_ptr->header.stamp.toSec();
@@ -41,6 +59,10 @@ void chatterCallback(const sensor_msgs::Imu::ConstPtr &imu_ptr) {
 }
 
 int main(int argc, char **argv) {
+  google::InitGoogleLogging(argv[0]);
+  google::ParseCommandLineFlags(&argc, &argv, true);
+  InitEstimatorOrExit();
+
   ros::init(argc, argv, "imu_eskf");
   ros::NodeHandle n;
   static_broadcaster = std::make_unique<tf2_ros::StaticTransformBroadcaster>();
